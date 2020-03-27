055fc918f6a02f2248db4d424b201886ce2a7003
Kerooker <leonardo.colman98@gmail.com>
2020-03-27 00:35:20 -0300

    Dummy ui test

--- app/src/androidTest/java/io/kotest/kotestexample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/kotest/kotestexample/ExampleInstrumentedTest.kt
@@ -1,11 +1,17 @@
 package io.kotest.kotestexample
 
+import androidx.test.core.app.launchActivity
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ExampleInstrumentedTest : FunSpec({
     
     test("Hello, Tests!") {
-        
+        val scenario = launchActivity<MainActivity>()
+        scenario.onActivity { 
+            it.tv.text shouldBe "Dummy!"
+        }
     }
     
 })
